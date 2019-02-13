df67ab645dd9aff046f3fea301febe01470fdb47
Thomas Pope <tpope0928@gmail.com>
2019-02-13 00:28:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Drew Pope"
+  end
+
+  get '/hometown' do
+    "My hometown is Gaithersburg, MD"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bertha"
+  end
 end
